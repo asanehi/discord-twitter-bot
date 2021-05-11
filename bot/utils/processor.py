4826f6227aeb80253f1bbf8c0dcd90c4252a2388
adityasanehi <singhsanehi26@gmail.com>
2021-05-11 19:59:05 +0530

    Update processor.py

--- bot/utils/processor.py
+++ bot/utils/processor.py
@@ -386,7 +386,7 @@ class Processor:
             icon_url=self.status_tweet["user"]["profile_image_url"],
         )
         self.embed.set_footer(
-            text="Twitter v1 | Adi x HeavyDrops",
+            text="Crypto-Twitter v1.1 | Adi x HeavyDrops",
             icon_url="https://i.imgur.com/mMkIcZD.png",
         )
 
